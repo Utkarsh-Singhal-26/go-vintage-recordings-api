b1c61a62d14c480967d813032dea921b8a7539f7
singhalutkarsh26 <singhalutkarsh26@gmail.com>
2024-03-30 11:25:02 +0000

    [UPDATED] GET ALL ALBUMS ROUTE

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	
+	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
 )
+
+type Album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
 
 type album struct {
 	ID     string  `json:"id"`
@@ -19,15 +32,63 @@ var albums = []album{
 }
 
 func main() {
+	cfg := mysql.Config{
+		User:	os.Getenv("DBUSER"),
+		Passwd:	os.Getenv("DBPASS"),
+		Net:	"tcp",
+		Addr:	"127.0.0.1:3306",
+		DBName:	"recordings",
+	}
+
+	var db *sql.DB
+	var err error
+
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal("ERR : ", err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal("PING : ", pingErr)
+	}
+
+	fmt.Println("Connected !")
+
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
+	router.GET("/albums", func(c *gin.Context) {
+		getAlbums(c, db)
+	})
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8000")
 }
 
-func getAlbums(c *gin.Context) {
+func getAlbums(c *gin.Context, db *sql.DB) {
+	rows, err := db.Query("SELECT * FROM album")
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var albums []Album
+
+	for rows.Next() {
+		var album Album
+		if err := rows.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		albums = append(albums, album)
+	}
+
+	if err := rows.Err(); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
